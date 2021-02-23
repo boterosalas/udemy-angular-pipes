419c06f748072a311f892e2def5d8eb4d0840a3c
boterosalas <boterosalas@gmail.com>
2021-02-23 11:51:58 -0500

    Hemos añadido algunos pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResolveEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Ironman';
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234123;
+  salario: number = 1234.5;
+
+  fecha: Date = new Date();
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
+  valorPromesa = new Promise<string>((resolve)=>{
+    setTimeout(() => {
+      console.log('Promesa');
+      resolve('Llegó la data')
+    }, 4500);
+    
+  })
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { registerLocaleData } from '@angular/common'
+import localEs from '@angular/common/locales/es'
+
+registerLocaleData(localEs)
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
